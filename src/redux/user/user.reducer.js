1a4637d09ecb6dea0348fe7b54362fb862a8a055
Armand van Blerk <armandavnblerk@protonmail.com>
2020-01-26 13:00:37 +0200

    upadating the app to redirect user to home when they are allready signed in

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,3 +1,5 @@
+import { UserActionTypes } from "./user.types";
+
 // declare the initial state the first time since redux wont know what the current state is
 const INITIAL_STATE = {
   currentUser: null
@@ -5,7 +7,7 @@ const INITIAL_STATE = {
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case "SET_CURRENT_USER":
+    case UserActionTypes.SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload
